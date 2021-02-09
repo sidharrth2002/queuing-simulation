7b46e4e1769fa2b1246d18f15adf6cd644107491
asmsanad <asmsanad@outlook.com>
2021-02-09 20:00:43 +0800

    counter v1

--- main.m
+++ main.m
@@ -74,18 +74,18 @@ function p = main(n)
     
     
     %Main Table:
-    t= 1;
+    t=1;
     mc=2;
     cin=1;
     temp(t)=0;
     randomIntervalTime(t)=0;
-    interarrivaltime(t)=0.25;
-    arrivaltime(t)=1;
+    interarrivaltime(t)=0;
+    arrivaltime(t)=0;
     customersInCenter(t)=0;
     timeEnteringCenter(t)=0;
     randomServiceTime(t)=0;
 
-    
+   
     %counter1
     counter1check=0;
     servicetime1(t)=0;
@@ -101,7 +101,9 @@ function p = main(n)
     waitingTime(t)=0;
     timeSpent(t)=0;
 
+
     while( t< n)
+        %set values:
         temp(t)=random(35, 42);
         randomIntervalTime(t)=random(1, 100);
         %interarrivaltime
@@ -114,11 +116,23 @@ function p = main(n)
         end
          
         %arrival time
-        if t == 1
-            arrivaltime(t) = 0;
-        else
+        arrivaltime(1)=0;
+        if t > 1
             arrivaltime(t) =  arrivaltime(t-1) + interarrivaltime(t);
+            
+            if arrivaltime(t) >=  timeServiceE1(t-1) 
+                counter1check=0;
+            else
+                counter1check=1; 
+            end
+            
+            if arrivaltime(t) >=  timeServiceE2(t-1) 
+                counter2check=0;
+            else
+                counter2check=1;
+            end
         end
+        
          
         %time entering the center
         if cin == mc
@@ -130,34 +144,62 @@ function p = main(n)
         %random number for servicetime
         randomServiceTime(t)=random(1,100);
         
-
+         % no one at c1 00 10 01  11
+        %c1 =1 c2 =0
+        %c1 =1 c2 =0
         %choosing counter
-        if counter1check == 0 %counter 1 is free
+        
+        if (counter1check == 0 & counter2check == 0) | (counter1check == 0 & counter2check == 1) %both free
+           %service time:
            for u=1:n-1
-             if randomServiceTime(t) > startrange1(u) & randomServiceTime(t) < endrange1(u)
+             if randomServiceTime(t) > startrange1(u) & randomServiceTime(t) < endrange1(u)%counter 1 is assigned
                  servicetime1(t) = u;
+                 break;
              end      
-             servicetime2(t) = 0;
            end
-           counter1check =1;
+           servicetime2(t) = 0;
+           %time service begins:
+           timeServiceB1(t)=arrivaltime(t);
+           timeServiceB2(t)=0;
+           %time service ends:
+           timeServiceE1(t)=timeServiceB1(t)+ servicetime1(t);
+           timeServiceE2(t)=0;
+           waitingTime(t)=0;
+           timeSpent(t) = timeServiceE1(t) - arrivaltime(t);
 
-           
-        elseif counter1check == 1 %counter 1 is full
+
+        elseif  counter1check == 1 & counter2check == 0 %1 full 2 free
            for u=1:n-1
-             if randomServiceTime(t) > startrange2(u) & randomServiceTime(t) < endrange2(u)
+             if randomServiceTime(t) > startrange2(u) & randomServiceTime(t) < endrange2(u) %counter 2 is assigned
                  servicetime2(t) = u;
-             end  
-            servicetime1(t) = 0;
+             end      
            end
-           counter2check =1;
+           servicetime1(t) = 0;
+           %time service begins:
+           timeServiceB2(t)=arrivaltime(t);
+           timeServiceB1(t)=0;
+           %time service ends:
+           timeServiceE2(t)=timeServiceB2(t)+ servicetime2(t);
+           timeServiceE1(t)=0;
+           waitingTime(t)=0;
+           timeSpent(t) = timeServiceE2(t) - arrivaltime(t);
            
-        elseif counter1check == 1 &  counter2check == 1 %both are full
-           disp('wait2');
+        elseif  counter1check == 1 & counter2check == 1 %both full
+           servicetime2(t) = 0;
+           servicetime1(t) = 0;
+           %time service begins:
+           timeServiceB2(t)=0;
+           timeServiceB1(t)=0;
+           %time service ends:
+           timeServiceE2(t)=0;
+           timeServiceE1(t)=0;
+           waitingTime(t)=0;
+           timeSpent(t) = 0;
             
+            
+     
        end
    
-        
-        
         t= t+1;
     end 
 
@@ -167,7 +209,7 @@ function p = main(n)
         fprintf('%2.0f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f\n', [j temp(j) randomIntervalTime(j) interarrivaltime(j)  arrivaltime(j) timeEnteringCenter(j) randomServiceTime(j) ]);
     end 
     
-            disp('j servicetime1 servicetime2');
+            disp('j servicetime1 timeServiceB1 timeServiceE1 servicetime2 timeServiceB2 timeServiceE2  waitingTime  timeSpent');
     for j=1:n-1
-        fprintf('%2.0f %10.3f %10.3f\n', [j  servicetime1(j) servicetime2(j) ]);
+        fprintf('%2.0f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f\n', [j  servicetime1(j) timeServiceB1(j) timeServiceE1(j) servicetime2(j) timeServiceB2(j) timeServiceE2(j)  waitingTime(j)  timeSpent(j) ]);
     end 
