bd2f9ac6d8b2ceb1bf7c7dabb8a731ecb2dd37c2
asmsanad <asmsanad@outlook.com>
2021-02-11 13:26:47 +0800

    max number is done

--- LCG.m
+++ LCG.m
@@ -1,11 +1,11 @@
 function output = LCG(min, max)
-    modulus = max + 1;
-    a = 2;
-    c = 0;
+    modulus = max+1;
+    a = 0;
+    c = random(min,max);
     persistent previousVal; 
     if isempty(previousVal);
-        previousVal = rand; 
+        previousVal =min; 
     end; 
     newVal = mod((a*previousVal + c), modulus);
     previousVal = newVal;
-    output = mod(newVal, (max-min + 1)) + min;
+    output = newVal;
--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 function p = main()
     % take input to choose generator
     disp('Please select a randomiser generator:');
-    disp('1-Standard Generator ');
+    disp('1-Standard Generator');
     disp('2-Linear Congruential Generator ');
     disp('3-Random Variate Generator ');
     rchooser = input('input: ');
@@ -12,23 +12,24 @@ function p = main()
     %InterArrival Table:
     i=1;
     iat(i)=i;
-    prob(i)= 1/n;
-    cdf(i)= 1/n; 
+    prob(i)= 1/5;
+    cdf(i)= 1/5; 
     startrange(i) = 1;
     endrange(i) = cdf(i)*100;
     incendrange=cdf(i)*100;
+    
 
     i=2;
-    while( i<= n)
+    while( i<= 5)
          iat(i)=i;
-         prob(i)=1/n;
+         prob(i)=1/5;
          cdf(i)= cdf(i-1) + prob(i);
          startrange(i)= endrange(i-1) + 1;
          endrange(i)= endrange(i-1) +  incendrange;
          i= i+1;
     end 
     disp('j iat prob cdf startrange endrange');
-    for j=1:n
+    for j=1:5
         fprintf('%2.0f %10.3f %10.3f %10.3f %10.3f %10.3f\n', [j iat(j) prob(j) cdf(j) startrange(j) endrange(j)]);
     end 
 
@@ -42,16 +43,22 @@ function p = main()
     
     x=5;
     while( i< x)
-         st1(i)=random(3, 9);
+         if rchooser == 1 
+             st1(i)=random(3,9);
+         elseif rchooser == 2
+            st1(i)= LCG(3,9);
+         else 
+            st1(i)= randomVariate(3,9);
+         end
          prob1(i)=0.25;
-         cdf1(i+1)= cdf(i) + prob1(i);
+         cdf1(i+1)= cdf1(i) + prob1(i);
          startrange1(i+1)= endrange1(i) + 1;
          endrange1(i+1)= endrange1(i) + 25;
          i= i+1;
     end 
         
     disp('Counter 1:');
-    disp('st prob cdf startrange endrange');
+    disp('j st prob cdf startrange endrange');
     for j=1:x-1
         fprintf('%2.0f %10.3f %10.3f %10.3f %10.3f %10.3f\n', [j st1(j) prob1(j) cdf1(j) startrange1(j) endrange1(j)]);
     end 
@@ -66,7 +73,13 @@ function p = main()
     endrange2(i) = 25;
     x=5;
         while( i< x)
-         st2(i)=random(3, 9);
+         if rchooser == 1 
+             st2(i)=random(3,9);
+         elseif rchooser == 2
+            st2(i)= LCG(3,9);
+         else 
+            st2(i)= randomVariate(3,9);
+         end
          prob2(i)=0.25;
          cdf2(i+1)= cdf2(i) + prob2(i);
          startrange2(i+1)= endrange2(i) + 1;
@@ -75,7 +88,7 @@ function p = main()
     end 
         
     disp('Counter 2:');
-    disp('st prob cdf startrange endrange');
+    disp('j st prob cdf startrange endrange');
     for j=1:x-1
         fprintf('%2.0f %10.3f %10.3f %10.3f %10.3f %10.3f\n', [j st2(j) prob2(j) cdf2(j) startrange2(j) endrange2(j)]);
     end 
@@ -89,7 +102,6 @@ function p = main()
     lastendtime1=0;
     lastendtime2=0;
     t=1;
-    cin=0;
     temp(t)=0;
     counter1check=0;
     counter2check=0;
@@ -118,17 +130,29 @@ function p = main()
         timeSpent(t)=0;
 
         %set values:
-        temp(t)=random(35, 42);
+        if rchooser == 1 
+             temp(t)=random(35, 42);
+        elseif rchooser == 2
+            temp(t)= LCG(35, 42);
+        else 
+            temp(t)= randomVariate(35, 42);
+        end
 
-
+        if rchooser == 1 
+             randomIntervalTime(t)=random(1, 100);
+        elseif rchooser == 2
+            randomIntervalTime(t)=LCG(1, 100);
+        else 
+            randomIntervalTime(t)=randomVariate(1, 100);
+        end
         
-        randomIntervalTime(t)=random(1, 100);
+       
         %interarrivaltime
-        for u=1:n-1
+        for u=1:5
              if t == 1
                 randomIntervalTime(t)=0;
                 interarrivaltime(t) = 0;
-             elseif randomIntervalTime(t) > startrange(u) & randomIntervalTime(t) < endrange(u)
+             elseif randomIntervalTime(t) >= startrange(u) & randomIntervalTime(t) <= endrange(u)
                 interarrivaltime(t) = u;
              end      
         end
@@ -137,29 +161,11 @@ function p = main()
         arrivaltime(1)=0;
         if t > 1
             arrivaltime(t) =  arrivaltime(t-1) + interarrivaltime(t); % breaks
-            if arrivaltime(t) >=  timeServiceE1(t-1) 
-                counter1check=0;
-                cin = cin + 1;
-                customersInCenter(t)=cin;
-            else
-                counter1check=1; 
-                cin = cin - 1;
-                customersInCenter(t)=cin;
-            end
-            
-            if arrivaltime(t) >=  timeServiceE2(t-1) 
-                counter2check=0;
-                 cin = cin + 1;
-                 customersInCenter(t)=cin;
-            else
-                counter2check=1;
-                cin = cin - 1;
-                customersInCenter(t)=cin;
-            end
         end
+
         
         %temp not allowed
-        if  temp(t) >= 38 & cin >= maxc
+        if  temp(t) >= 38 
             timeEnteringCenter(t)=0;
             randomServiceTime(t)=0;
    
@@ -175,17 +181,67 @@ function p = main()
  
             waitingTime(t)=0;
             timeSpent(t)=0;
+
             
         else  %temp allowed 
-        
+
+            %counters switching
+            if t > 1
+                if arrivaltime(t) >=  timeServiceE1(t-1) 
+                    counter1check=0;
+                else
+                    counter1check=1; 
+                end
+            
+                if arrivaltime(t) >=  timeServiceE2(t-1) 
+                    counter2check=0;
+                else
+                    counter2check=1;
+
+                end
+                
+                %number of customers in center
+                cin=0;
+                carrivaltime = arrivaltime(t);
+                for u=1:t-1  
+                    if carrivaltime < timeServiceE1(u)
+                        cin =cin +1;
+                    end      
+                    if carrivaltime < timeServiceE2(u)
+                        cin =cin +1;
+                    end   
+                end
+                customersInCenter(t)=cin;
+                
+                % check max customers
+                if cin <= maxc
+                     timeEnteringCenter(t)=arrivaltime(t);
+                else
+                    if lastendtime1 <= lastendtime2
+                         timeEnteringCenter(t)=lastendtime1;
+                    else
+                        timeEnteringCenter(t)=lastendtime2;
+                    end
+                end
+                                    
+            end
+
             %random number for servicetime
-            randomServiceTime(t)=random(1,100);
-        
+            if rchooser == 1 
+               randomServiceTime(t)=random(1,100);
+            elseif rchooser == 2
+                 randomServiceTime(t)=LCG(1, 100);
+            else 
+                 randomServiceTime(t)=randomVariate(1, 100);
+            end
+            
+
+            
             %choosing counter
             if (counter1check == 0 & counter2check == 0) | (counter1check == 0 & counter2check == 1) %both free
                 %service time:
                 for u=1:x-1
-                    if randomServiceTime(t) > startrange1(u) & randomServiceTime(t) < endrange1(u)%counter 1 is assigned
+                    if randomServiceTime(t) >= startrange1(u) & randomServiceTime(t) <= endrange1(u)%counter 1 is assigned
                         servicetime1(t) = u;
                     end      
                 end
@@ -199,11 +255,12 @@ function p = main()
                 timeServiceE2(t)=0;
                 waitingTime(t)=0;
                 timeSpent(t) = timeServiceE1(t) - arrivaltime(t);
+                
 
 
             elseif  counter1check == 1 & counter2check == 0 %1 full 2 free
                 for u=1:x-1
-                    if randomServiceTime(t) > startrange2(u) & randomServiceTime(t) < endrange2(u) %counter 2 is assigned
+                    if randomServiceTime(t) >= startrange2(u) & randomServiceTime(t) <= endrange2(u) %counter 2 is assigned
                         servicetime2(t) = u;
                     end      
                 end
@@ -217,11 +274,12 @@ function p = main()
                 timeServiceE1(t)=0;
                 waitingTime(t)=0;
                 timeSpent(t) = timeServiceE2(t) - arrivaltime(t);
+                
            
             elseif  counter1check == 1 & counter2check == 1 %both full
                 if lastendtime1 < lastendtime2 %counter 1 will finish first
                     for u=1:x-1
-                        if randomServiceTime(t) > startrange1(u) & randomServiceTime(t) < endrange1(u) %counter 1 is assigned
+                        if randomServiceTime(t) >= startrange1(u) & randomServiceTime(t) <= endrange1(u) %counter 1 is assigned
                             servicetime1(t) = u;
                         end      
                     end
@@ -235,10 +293,11 @@ function p = main()
                     waitingTime(t)=lastendtime1-arrivaltime(t);
                     timeSpent(t) = timeServiceE1(t) - arrivaltime(t);
                     lastendtime1= timeServiceE1(t);
+                    
     
                 else %counter 2 will finish first
                     for u=1:x-1
-                        if randomServiceTime(t) > startrange2(u) & randomServiceTime(t) < endrange2(u) %counter 2 is assigned
+                        if randomServiceTime(t) >= startrange2(u) & randomServiceTime(t) <= endrange2(u) %counter 2 is assigned
                             servicetime2(t) = u;
                         end      
                     end
